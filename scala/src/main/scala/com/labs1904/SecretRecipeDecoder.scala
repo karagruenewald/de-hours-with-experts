e14f89725cc0c642ee54a707ea2d87129973e482
Kara Gruenewald <karagruenewald@gmail.com>
2022-03-31 16:37:33 -0600

    implemented decodeString and decodeIngredient, working on main

--- scala/src/main/scala/com/labs1904/SecretRecipeDecoder.scala
+++ scala/src/main/scala/com/labs1904/SecretRecipeDecoder.scala
@@ -1,6 +1,9 @@
 package com.labs1904
 
+import java.io.PrintWriter
 import scala.collection.immutable.HashMap
+import scala.io.Source
+import scala.reflect.io.File
 
 /**
  * An ingredient has an amount and a description.
@@ -54,20 +57,45 @@ object SecretRecipeDecoder {
    * @param str A caesar-encoded string.
    * @return
    */
-  def decodeString(str: String): String  = ???
+  def decodeString(str: String): String  = {
+    val listOfChars = str.toCharArray
+    var decodedString : String = ""
+    listOfChars.foreach(c =>
+      if(SecretRecipeDecoder.ENCODING.get(c.toString) != None){
+        val decodedLetter = SecretRecipeDecoder.ENCODING.get(c.toString)
+        decodedString += decodedLetter.get
+      }
+      else {
+        decodedString += " "
+      })
+    decodedString
+  }
+
 
   /**
    * Given an ingredient, decode the amount and description, and return a new Ingredient
    * @param line An encoded ingredient.
    * @return
    */
-  def decodeIngredient(line: String): Ingredient = ???
+  def decodeIngredient(line: String): Ingredient = {
+    val encoded = line.split("#")
+    val ingredient = new Ingredient(decodeString(encoded(0)), decodeString(encoded(1)))
+    ingredient
+  }
+
 
   /**
    * A program that decodes a secret recipe
    * @param args
    */
   def main(args: Array[String]): Unit = {
-    // TODO: implement me
+    val filename = "src/main/resources/secret_recipe.txt"
+    val pw = new PrintWriter(new File("decoded_recipe.txt"))
+
+    for (line <- Source.fromFile(filename).getLines) {
+      val ingredient = decodeIngredient(line)
+      pw.write(s"${ingredient.amount} ${ingredient.description}")
+    }
+    pw.close
   }
 }
